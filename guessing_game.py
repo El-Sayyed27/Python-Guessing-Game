4ab089b27a1d4ee239608790c3fbe86a4455d46f
El-Sayyed <athumansaid850@gmail.com>
2026-03-23 01:22:43 +0300

    Added correct/wrong sound effects

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,7 @@
 import random
+from playsound import playsound
 number = random.randint(1,100)
+
 
 print("GUESS A NUMBER")
 attempts = 6
@@ -15,14 +17,17 @@ while True:
         print("Invalid input! Please enter a number")
 
     if guess < number:
+        playsound(r"sounds/wrong-answer.wav")
         print("Guess is too low, "+str(attempts)+" attempts left")
         attempts = int(attempts) - 1
         continue
     elif guess > number:
+        playsound(r"sounds/wrong-answer.wav")
         print("Guess is too high, "+str(attempts)+" attempts left")
         attempts = int(attempts) - 1
         continue
     else:
+        playsound(r"sounds/correct-answer.wav")
         print("Correct, "+str(number)+" was the random number!")
         break
     
