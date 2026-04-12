7e26773e4cf9f3c4ddd7a29506566d3b798ddec0
El Sayyed <athumansaid850@gmail.com>
2026-04-12 17:46:20 +0300

    Removed correct/wrong sound effect

--- guessing_game.py
+++ guessing_game.py
@@ -1,5 +1,4 @@
 import random
-from playsound import playsound
 from colorama import Fore
 number = random.randint(1,100)
 
@@ -9,26 +8,23 @@ attempts = 6
 
 while True:
     if int(attempts) == 0:
-        print(Fore.RED +"Sorry, You are out of attemps!")
+        print(Fore.RED +"Sorry, You are out of attemps!" + Fore.RESET)
         break
 
     try :
-        guess = int(input(Fore.CYAN +"Enter your guess: "))
+        guess = int(input(Fore.CYAN +"Enter your guess: " + Fore.RESET))
     except ValueError:
-        print(Fore.RED +"Invalid input! Please enter a number")
+        print(Fore.RED +"Invalid input! Please enter a number" + Fore.RESET)
 
     if guess < number:
-        playsound(r"sounds/wrong-answer.wav")
-        print(Fore.RED +"Guess is too low, "+str(attempts)+" attempts left")
+        print(Fore.RED +"Guess is too low, "+str(attempts)+" attempts left" + Fore.RESET)
         attempts = int(attempts) - 1
         continue
     elif guess > number:
-        playsound(r"sounds/wrong-answer.wav")
-        print(Fore.RED +"Guess is too high, "+str(attempts)+" attempts left")
+        print(Fore.RED +"Guess is too high, "+str(attempts)+" attempts left" + Fore.RESET)
         attempts = int(attempts) - 1
         continue
     else:
-        playsound(r"sounds/correct-answer.wav")
-        print(Fore.GREEN +"Correct, "+str(number)+" was the random number!")
+        print(Fore.GREEN +"Correct, "+str(number)+" was the random number!" + Fore.RESET)
         break
     
